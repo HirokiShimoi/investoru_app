4af9d942de6f27775d71b5312c873e79e097ff77
Hiroki Shimoie <apelox1gmail.com>
2023-08-29 01:16:41 +0900

    API追加

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views  # ここでviews.pyをインポート
 
 urlpatterns = [
+    path('api/inventory/reorder/', views.UnderOrderLine.as_view(), name='under-order-products'),
     path('api/products/', views.ProductListCreateView.as_view(), name='product-list-create'),
+    path('api/inventory/', views.InventoryListCreateView.as_view(), name='inventory-list-create'),
+    path('api/orderline/', views.OrderLineListCreateView.as_view(), name='orderline-list-create'),
+    path('api/products/<str:product_code>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('api/inventory/<str:product_code>/', views.ProductDetailView.as_view(), name='inventory-detail'),
+    path('api/orderline/<str:product_code>/', views.ProductDetailView.as_view(), name='orderline-detail'),
+    path('api/inventory/update/<str:product_code>/', views.InventoryUpdateView.as_view(), name='inventory-update'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from rest_framework import status, views
+from django.db.models import F
+from rest_framework import status, views,generics
 from rest_framework.response import Response
 from .models import Product,Inventory,OrderLine
 from .serializers import ProductSerializer, InventorySerializer,OrderLineSerializer
+from django.shortcuts import get_object_or_404,render
+
 
 class ProductListCreateView(views.APIView):
 
@@ -17,3 +19,71 @@ class ProductListCreateView(views.APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'product_code'
+    
+
+class InventoryListCreateView(views.APIView):
+
+    def get(self,request):
+        inventory = Inventory.objects.all()
+        serializer = InventorySerializer(inventory, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = InventorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class InventoryDetailView(generics.RetrieveAPIView):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+    lookup_field = 'product_code'
+
+class InventoryUpdateView(views.APIView):
+    def put(self,request,product_code):
+        inventory = get_object_or_404(Inventory, product_code=product_code)
+        serializer = InventorySerializer(inventory, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class OrderLineListCreateView(views.APIView):
+
+    def get(self,request):
+        orderline = OrderLine.objects.all()
+        serializer = OrderLineSerializer(orderline, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = OrderLineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class OrderLineDetailView(generics.RetrieveAPIView):
+    queryset = OrderLine.objects.all()
+    serializer_class = OrderLineSerializer
+    lookup_field = 'product_code'
+
+
+class UnderOrderLine(views.APIView):
+    def get(self,request):
+        print("This is a test.")
+        underorderproducts =Inventory.objects.annotate(
+            reorder_point = F('product__orderline__reorder_point')
+        ).filter(current_stock__lte=F('reorder_point'))
+
+        print("Under Order Products: ", underorderproducts.query)
+
+        serializer = InventorySerializer(underorderproducts, many=True)
+        return Response(serializer.data)
