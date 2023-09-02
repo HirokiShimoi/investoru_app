5f7f0f88bd76c343ce8fc71cff08c25caeabca79
HirokiShimoi <you@example.com>
2023-09-02 18:39:25 +0900

    エンドポイント説明追加

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views  
+from . import views  # ここでviews.pyをインポート
 
 urlpatterns = [
-    path('api/inventory/reorder/', views.UnderOrderLine.as_view(), name='under-order-products'),
-    path('api/products/', views.ProductListCreateView.as_view(), name='product-list-create'),
-    path('api/inventory/', views.InventoryListCreateView.as_view(), name='inventory-list-create'),
+    path('api/inventory/reorder/', views.UnderOrderLine.as_view(), name='under-order-products'), #カテゴリ別の発注点より少ない在庫アイテム一覧（ページネーション済み）
+    path('api/products/', views.ProductListCreateView.as_view(), name='product-list-create'), #全商品一覧
+    path('api/inventory/', views.InventoryListCreateView.as_view(), name='inventory-list-create'),#在庫と自社コード一覧
     path('api/orderline/', views.OrderLineListCreateView.as_view(), name='orderline-list-create'),
-    path('api/products/<str:product_code>/', views.ProductDetailView.as_view(), name='product-detail'),
-    path('api/inventory/<str:product_code>/', views.ProductDetailView.as_view(), name='inventory-detail'),
-    path('api/orderline/<str:product_code>/', views.ProductDetailView.as_view(), name='orderline-detail'),
+    path('api/products/<str:product_code>/', views.ProductDetailView.as_view(), name='product-detail'),#自社コードでソートしたアイテム
+    path('api/inventory/<str:product_code>/', views.InventoryDetailView.as_view(), name='inventory-detail'),#自社コードでソートした在庫アイテム
+    path('api/orderline/<str:product_code>/', views.OrderLineDetailView.as_view(), name='orderline-detail'),
     path('api/inventory/update/<str:product_code>/', views.InventoryUpdateView.as_view(), name='inventory-update'),
-    path('api/create_user/',views.UserCreateView.as_view(),name = 'user-create'),
+    path('api/create_user/', views.UserCreateView.as_view(),name = 'user-create'),
     
 ]
