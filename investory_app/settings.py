59ae0336755805e1019f0e01904da76e9181f4e7
Hiroki Shimoie <apelox1gmail.com>
2024-01-07 23:14:43 +0900

    デプロイ前のrequirements.txtの変更

--- investory_app/settings.py
+++ investory_app/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6i==!t208!k8n!ehb#rd6nwpl_hbk*+6j5o=-wm@kc&umsg%u'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["18.176.61.148"]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
